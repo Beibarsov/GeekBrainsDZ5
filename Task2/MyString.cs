8b69d10eb3cae06e720110d0f098f33d2f1bb2b3
Виталий Никонов <BarsStudio@gmail.com>
2018-08-03 17:54:56 +0800

    123

---
+++ Task2/MyString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    class MyString
+    {
+        string text;
+
+        /// <summary>
+        /// Конструктор с пустой строкой
+        /// </summary>
+        public MyString()
+        {
+            this.text = string.Empty;
+        }
+
+        /// <summary>
+        /// Конструктор, где значение строки передаётся параметром
+        /// </summary>
+        /// <param name="text">Значение строки</param>
+        public MyString(string text)
+        {
+            this.text = text;
+        }
+
+        /// <summary>
+        /// Выводит из текста только те слова, где не больше требуемого букв
+        /// </summary>
+        public void WriteNeBoleeBukv()
+        {
+
+            Regex myregex = new Regex(@"[а-яА-Я0-9]+\>");
+            myregex.
+                Console.WriteLine(str);
+            //Console.WriteLine(text.Length);
+            
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2. Разработать методы для решения следующих задач.Дано сообщение:
+//а) Вывести только те слова сообщения, которые содержат не более чем n букв;
+//б) Удалить из сообщения все слова, которые заканчиваются на заданный символ;
+//в) Найти самое длинное слово сообщения;
+//г) Найти все самые длинные слова сообщения.
+//Постараться разработать класс MyString.
+
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString mystring = new MyString("Шла Саша по шоссе и цйуйуйуйц");
+            mystring.WriteNeBoleeBukv();
+        }
+    }
+}
