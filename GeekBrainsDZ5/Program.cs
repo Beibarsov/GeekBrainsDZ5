76ac4712356e566d6b19ec515c434b7546351687
Виталий Никонов <BarsStudio@gmail.com>
2018-08-03 17:20:06 +0800

    123

--- GeekBrainsDZ5/Program.cs
+++ GeekBrainsDZ5/Program.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+//1. Создать программу, которая будет проверять корректность ввода логина.
+//   Корректным логином будет строка от 2-х до 10-ти символов, содержащая только буквы или цифры,
+//    и при этом цифра не может быть первой.
+//а) без использования регулярных выражений;
+//б) ** с использованием регулярных выражений.
+
+
 
 namespace GeekBrainsDZ5
 {
@@ -10,6 +19,26 @@ namespace GeekBrainsDZ5
     {
         static void Main(string[] args)
         {
+            // myReg = new Regex(@"[A-Z]{2,8}");
+            //if (myReg.IsMatch("QQQQQQQQQQQQQQQQQQQQQ"))
+            //   Console.WriteLine("TRUE");
+
+            //а) без использования регулярных выражений;
+            //string str = Console.ReadLine();
+            //char mychar = str[0];
+            //if (str.Length > 1 && str.Length < 11 && char.GetNumericValue(mychar) == -1)
+            //{
+            //    Console.WriteLine("Логин корректный");
+            //}
+
+            //б) ** с использованием регулярных выражений
+            Regex myregex = new Regex(@"[A-Za-z]+([A-Za-z0-9]{1,9})$");
+            if (myregex.IsMatch("1UserUserUser"))
+            {
+                Console.WriteLine("Логин корректный");
+            }
+
+
         }
     }
 }
